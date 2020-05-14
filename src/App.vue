1864d92c5371e09c97690aba846aabfb5b84bed8
pere.verges <pere.verges@jediupc.com>
2020-05-14 10:25:28 +0200

    PDF pressupost creation

--- src/App.vue
+++ src/App.vue
@@ -15,6 +15,9 @@
         <div class="col-md-auto align-self-start" style="margin-right: -4px; padding-left: 0; margin-bottom: 16px">
           <button class="btn btn-outline-primary align-self-start" style="float:left;" @click="saveData">Save</button>
         </div>
+        <div class="col-md-auto align-self-start" style="margin-right: -4px; padding-left: 0; margin-bottom: 16px">
+          <button class="btn btn-outline-primary align-self-start" style="float:left;" @click="createPdf">PDF</button>
+        </div>
         <div class="col-md-auto" style="padding-left: 0">
           <div class="custom-file" style="max-width: 170px; float:left;">
             <input type="file" class="custom-file-input"  style="margin-top: 8px" @change="loadBudget">
@@ -33,6 +36,7 @@ import Parameters from "@/components/Parameters/Parameters";
 import Result from "@/components/Parameters/Result";
 import Roles from "@/components/Parameters/Roles";
 import FixedCostsHolder from "@/components/FixedCosts/FixedCostsHolder";
+import jsPDF from 'jspdf'
 
 export default {
   name: 'App',
@@ -59,6 +63,61 @@ export default {
       document.body.appendChild(downloadAnchorNode); // required for firefox
       downloadAnchorNode.click();
       downloadAnchorNode.remove();
+    },
+    createPdf(){
+      let budget = this.$store.getters.getAllData;
+      let doc = new jsPDF();
+      let pageWidth = doc.internal.pageSize.width || doc.internal.pageSize.getWidth();
+      let y = 30;
+      let x = 10;
+      doc.setFontSize(28);
+      doc.setFontStyle('bold');
+      doc.text("Pressupost", x, y);
+      y += 15;
+      console.log(budget.functionalitiesObject.functionalities.length);
+      let i = 0;
+      while (i < budget.functionalitiesObject.functionalities.length){
+        doc.setFontStyle('bold');
+        doc.setFontSize(16);
+        x = 10;
+        doc.text(budget.functionalitiesObject.functionalities[i].name, x, y);
+        doc.text(budget.functionalitiesObject.functionalities[i].cost.toString(), pageWidth-10, y, 'right');
+        y += 8;
+        let j = 0;
+        x = 20;
+        doc.setFontSize(12);
+        doc.setFontStyle('normal');
+        while(j < budget.functionalitiesObject.functionalities[i].tasks.length){
+          doc.text("- " + budget.functionalitiesObject.functionalities[i].tasks[j].name, x, y);
+          y += 8;
+          j = j+1;
+        }
+        i = i+1;
+      }
+      let k = 0;
+      if(budget.fixedCostsObject.fixedCosts !== undefined) {
+        while (k < budget.fixedCostsObject.fixedCosts.length) {
+          doc.setFontStyle('bold');
+          doc.setFontSize(16);
+          x = 10;
+          doc.text(budget.fixedCostsObject.fixedCosts[k].name, x, y);
+          doc.text(budget.fixedCostsObject.fixedCosts[k].cost.toString(), pageWidth - 10, y, 'right');
+          y += 8;
+          k += 1;
+        }
+      }
+      x = 10;
+      doc.setFontStyle('bold');
+      doc.setFontSize(16);
+      doc.line(10,y,pageWidth-10,y);
+      y += 12;
+      doc.text("Total cost", x, y);
+      doc.text(budget.totalCost.toString(), pageWidth-10,y, 'right');
+      y += 10;
+      doc.text("Total hours", x, y);
+      doc.text(budget.totalCost.toString(), pageWidth-10,y, 'right');
+
+      doc.save('pressupost.pdf');
     },
     loadBudget(event){
       const file = event.target.files[0];
